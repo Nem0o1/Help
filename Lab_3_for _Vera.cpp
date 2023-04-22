af25cff5f735b14b41183a1023cc68f323e7cdb0
Ivan <ivan.potapov.2004@yandex.ru>
2023-04-22 18:02:55 +0300

    it_work

---
+++ Lab_3_for _Vera.cpp
@@ -0,0 +1,135 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Base
+{
+private:
+    int** masik;
+    int str;
+    int stlb;
+    int size;
+    int sum_pch = 0;
+    int* tiny;
+    int hal_fmasik;
+    int temp;
+    int counter = 0;
+    int a = 0;
+public:
+    Base(){}
+    void Massik() {
+        int** masik = new int* [str];
+        for (int i = 0; i < str; i++) {
+            masik[i] = new int[stlb];
+        }
+        for (int i = 0; i < str; i++) {
+            for (int j = 0; j < stlb; j++) {
+                masik[i][j] = rand() % 13;
+            }
+        }
+        for (int i = 0; i < str; i++) {
+            for (int j = 0; j < stlb; j++) {
+                cout << masik[i][j] << "\t";
+            }
+            cout << endl;
+        }
+    }
+    void CountProstieChisla() {
+        for (int i = 0; i < str; i++) {
+            for (int j = 0; j < stlb; j++) { // перебор массива
+                int counter = 0;
+                for (int m = 1; m < (masik[i][j] / 2 + 1); m++) {
+                    if (masik[i][j] % m == 0) {
+                        counter++;
+                    }
+                }
+                if (counter == 1) {
+                    cout << masik[i][j] << "- Простое число" << endl;
+                    sum_pch++;
+                }
+                else {
+                    cout << masik[i][j] << "- Составное число" << endl;
+                }
+
+            }
+        }
+        cout << sum_pch << " - сумма простых чисел" << endl;
+    }
+    void StolbicVMassiv() {
+        int half_masik = str * stlb / 2;
+        int* tiny{ new int[stlb] };
+        if (sum_pch >= half_masik) {
+            for (int i = 0; i < str; i++) {
+                tiny[i] = masik[i][stlb - 1];
+            }
+            cout << "Последний столбик: ";
+            for (int i = 0; i < stlb; ++i)
+            {
+                cout << tiny[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+    void Sortirovka() {
+        for (int i = 1; i < stlb; ++i){
+            for (int r = 0; r < stlb - i; r++){
+                if (tiny[r] < tiny[r + 1]){
+                    // Обмен местами
+                    int temp = tiny[r];
+                    tiny[r] = tiny[r + 1];
+                    tiny[r + 1] = temp;
+                }
+            }
+        }
+
+        /* отсортированный массив */
+        cout << "Отсортированный столбик: ";
+        for (int i = 0; i < stlb; ++i)
+        {
+            cout << tiny[i] << " ";
+        }
+        cout << endl;
+
+    }
+
+    ~Base() {
+
+            delete[] masik;
+            //функция создания массива
+            //функция заполнения массива
+            //функция выполняющая задание
+            //Деструктор базового класса для очищения памяти для массива
+
+    }
+};
+
+class Child : public Base // класс child наследуется от Base и имеет все те же функции, что и базовый класс
+{
+public:
+    Child(int str, int stlb) {
+        Massik();
+        CountProstieChisla();
+        StolbicVMassiv();
+        Sortirovka();
+    }
+
+    //Конструктор вызывающий все функции внутри из базового класса
+    //Деструктор производного класса
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int str;
+    cout << "Количество строк?: ";
+    cin >> str;
+    int stlb;
+    cout << "Количество столбцов?: ";
+    cin >> stlb;
+    //ввод размерности массива
+    //создание класса Child и вызов его конструктора
+    Child a(str,stlb);
+    return 0;
+}
